893215223b5ec3ce53e8f45850fcf089dde98fd1
Nataly-Wambui <wambuinatalygakenye@gmail.com>
2024-03-06 21:41:25 +0300

    Add file

---
+++ strings.js
@@ -0,0 +1,29 @@
+// Question 1
+let word= "extravaganza"
+console.log(word.substring(8,12))
+
+// Question 2
+let origString= "The quick fox jumped over the lazy dog";
+let stringToAdd= "eat";
+let indexPosition= 4;
+let newString= origString.substring(0, indexPosition)+stringToAdd+origString.substring(indexPosition);
+console.log({newString});
+
+// Question 3
+const story= "The quick brown fox jumps over the lazy dog";
+console.log((story.match(/the/)).length);
+console.log((story.match(/brown/)).length);
+
+//Question 4
+let nameOne= "wonderful";
+console.log(nameOne.toUpperCase());
+let nameTwo= "amaizing";
+let nameThree= "UndERneath";
+console.log(nameTwo.toLowerCase(), nameThree.toLowerCase())
+let nameFour= "A wonderful world"
+
+//Question 5
+const string1= "The pupils are reading in the library";
+console.log(string1.search("are"));
+const string2= "The child was sitting on the table before it fell";
+console.log(string2.search("sitting"));
